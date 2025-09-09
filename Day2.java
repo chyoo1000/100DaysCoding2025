80bbece01794c82875c703d1a629950156d280c4
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-09-09 14:17:18 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,7 @@
+public class Day2 {
+    public static void main(String[] args) {
+        System.out.print("Nama:");    
+        System.out.println(" Cahyo Susanto Basuki");  
+        System.out.printf("Umur: %d tahun",  18);
+    }
+}
