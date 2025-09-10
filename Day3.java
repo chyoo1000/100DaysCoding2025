e82c5fed90f30d41369940d46a74d10c81ea6a16
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-09-10 22:28:37 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,7 @@
+public class Day3 {
+    public static void main(String[] args) {
+        System.out.println("=== Day 3 ===");
+        System.out.println("Cahyo Susanto Basuki");
+        System.out.println("Day 3 menuju 100DaysofCoding");
+    }
+}
