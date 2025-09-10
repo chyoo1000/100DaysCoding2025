c682ed30fa825f1a88eb9e0b85c2704c53c61c0d
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-09-10 22:40:06 +0800

    Day3.java

--- Day3.java
+++ Day3.java
@@ -1,7 +1,13 @@
 public class Day3 {
     public static void main(String[] args) {
-        System.out.println("=== Day 3 ===");
-        System.out.println("Cahyo Susanto Basuki");
-        System.out.println("Day 3 menuju 100DaysofCoding");
+        System.out.print("Nama:");    
+        // print = mencetak teks tanpa ganti baris
+
+        System.out.println(" Cahyo Susanto Basuki");  
+        // println = mencetak teks lalu ganti baris
+
+        System.out.printf("Umur: %d tahun",  18);
+        // printf = mencetak teks dengan format, %d = angka (integer)
+        // saya menggunakan commit Day2 untuk materi di Day3
     }
 }
