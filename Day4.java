6345d24fdb23e96de30d4ca5f19238bdb6342df7
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-09-11 11:33:09 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,8 @@
+public class Day4 {
+    public static void main(String[] args) {
+        System.out.println("Aku\nDia");   
+        System.out.println("Aku\tDia");  
+        System.out.println("Aku \"Dia\""); 
+        System.out.println("Aku \\ Dia");
+    }
+}
