af900fc5f42ffee136cb291324b3725b97113350
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-09-12 18:23:37 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,16 @@
+public class Day5 {
+    public static void main(String[] args) {
+        System.out.println("=== BYTE ===");
+        System.out.println("Nilai Terkecil : " + Byte.MIN_VALUE);
+        System.out.println("Nilai Terbesar : " + Byte.MAX_VALUE);
+        System.out.println("\n=== SHORT ===");
+        System.out.println("Nilai Terkecil : " + Short.MIN_VALUE);
+        System.out.println("Nilai Terbesar : " + Short.MAX_VALUE);
+        System.out.println("\n=== INT ===");
+        System.out.println("Nilai Terkecil : " + Integer.MIN_VALUE);
+        System.out.println("Nilai Terbesar : " + Integer.MAX_VALUE);
+        System.out.println("\n=== LONG ===");
+        System.out.println("Nilai Terkecil : " + Long.MIN_VALUE);
+        System.out.println("Nilai Terbesar : " + Long.MAX_VALUE);
+    }
+}
