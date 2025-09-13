814a79edde8954708ee5c4e58373e3c5b80b4ab6
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-09-13 19:57:28 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,10 @@
+public class Day6 {
+    public static void main(String[] args) {
+        System.out.println("=== Double ===");
+        System.out.println("Nilai Terkecil : " + Double.MIN_VALUE);
+        System.out.println("Nilai Terbesar : " + Double.MAX_VALUE);
+        System.out.println("\n=== Float ===");
+        System.out.println("Nilai Terkecil : " + Float.MIN_VALUE);
+        System.out.println("Nilai Terbesar : " + Float.MAX_VALUE);
+    }
+}
