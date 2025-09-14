872df9e42f7d05c61c960d162ae6b0ab43b03331
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-09-14 16:11:44 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,10 @@
+public class Day7 {
+    public static void main(String[] args) {
+        char gedungB = 'B';
+        boolean benar = true;
+        double tingkatRuang = 3.21;
+        
+        System.out.println("Apa benar " + gedungB + tingkatRuang + " itu Gedung B?");
+        System.out.println("Jawaban: " + benar);
+    }
+}
