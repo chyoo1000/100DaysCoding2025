8093be557344d04cdf932ceafdc56ce737aece40
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-09-15 20:25:50 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,11 @@
+public class Day8 {
+	public static void main(String[] args) {
+		
+//	=== Contoh Wifi ===
+		String namaDevice = "Redmi Note 11";
+		String pw = "12345678";
+		
+		System.out.println("Nama Device: "+ namaDevice);
+		System.out.println("Password: "+ pw);
+	}
+}
