abd5aef1d2b25ec39bee197d8893a588b409902d
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-09-16 22:05:16 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,28 @@
+public class Day9 {
+	public static void main(String[] args) {
+//deklarasi
+		String nama;
+		int umur;
+//artinya kita membuat variabel baru dengan tipe data tertentu, tanpa harus langsung memberi nilai
+
+//inisialisasi	
+		nama = "Cahyo Susanto Basuki";
+		umur = 18;
+//artinya memberi nilai pertama kali pada variabel. bisa dilakukan langsung saat deklarasi atau setelahnya
+		
+//hasil inisialisasi
+        System.out.println("Inisialisasi");
+		System.out.println("Nama: "+ nama);
+		System.out.println("Umur: "+ umur);
+		
+//update variabel
+		nama = "@chyoo1000";
+		umur = 18; //menolak tua:v
+//artinya mengubah nilai variabel yang sudah ada
+
+//hasil setelah update
+        System.out.println("\nUpdate Variabel");		
+		System.out.println("Nama: "+ nama);
+		System.out.println("Umur: "+ umur);
+	}
+}
