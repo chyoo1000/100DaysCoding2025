26167ab8c379306fb6c65ef668d46dcd46c3ab8f
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-09-17 18:24:34 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,9 @@
+public class Day10 {
+	public static void main(String[] args) {
+		
+		final int angka = 100;
+		//constanta adalah variabel yang nilainya tidak bisa diubah setelah pertama kali diberi nilai. constanta dibuat dengan keyword final
+		
+		System.out.println(angka);
+	}
+}
