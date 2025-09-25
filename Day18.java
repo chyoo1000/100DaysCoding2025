8407dc2f3092290e42f1332c94f9c3174121ceb9
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-09-25 12:26:09 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,17 @@
+package Day18;
+
+public class Day18 {
+    public static void main(String[] args) {
+        byte angkaByte = 100;
+        int angkaInt = angkaByte;
+        long angkaLong = angkaInt;
+        float angkaFloat = angkaLong;
+        double angkaDouble = angkaFloat;
+        
+        System.out.printf("Byte     :%d\n", angkaByte);
+        System.out.printf("Int      :%d\n", angkaInt);
+        System.out.printf("Long     :%d\n", angkaLong);
+        System.out.printf("Float    :%.2f\n", angkaFloat);
+        System.out.printf("Double   :%.2f\n", angkaDouble);
+            }
+}
