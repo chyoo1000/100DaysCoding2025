2e2c6272413c1424540377009be25eb3ba95ff58
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-09-26 17:42:12 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,8 @@
+public class Day19 {
+	public static void main(String[] args) {
+		int angkaInt = 130;
+		byte angkaByte = (byte) angkaInt;
+		System.out.printf("Int	:%d\n", angkaInt);
+		System.out.printf("Byte	:%d\n", angkaByte);
+	}
+}
