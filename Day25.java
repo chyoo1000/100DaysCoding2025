313d1bffd5b150f80aac143b9593ecc6c3a26e59
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-10-02 19:53:21 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,13 @@
+public class Day25 {
+	public static void main(String[] args) {
+		int i = 10;
+		int d = 10;
+		System.out.printf("angka awal: 10\n\n");
+		
+		i++;
+		System.out.printf("increment(++): %d", i);
+		
+		d--;
+		System.out.printf("\ndecrement(--): %d\n", d);
+}
+}
