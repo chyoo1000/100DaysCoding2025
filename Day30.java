44119392a17e6915dd4f28b7608534aabf5938c5
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-10-07 20:26:03 +0800

    Day30.java

---
+++ Day30.java
@@ -0,0 +1,13 @@
+public class Day30 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a <= b);
+        System.out.println(a >= b);
+
+        int c = 10;
+        System.out.println(a <= c);
+        System.out.println(a >= c);
+    }
+}
