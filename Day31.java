90d4f4e9560d6b733f27f6eb18be03b8bb2f9e35
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-10-08 22:09:43 +0800

    Day31.java

---
+++ Day31.java
@@ -0,0 +1,11 @@
+public class Day31 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a > 5 && b > 10);
+        System.out.println(a > 15 && b > 10);
+        System.out.println(a > 5 && b < 10);
+        System.out.println(a < 5 && b < 10);
+    }
+}
