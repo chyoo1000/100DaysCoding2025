fc53e5d428c7abc2b99cbb8d6a58ab6df6e59880
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-10-09 20:50:52 +0800

    Day32.java

---
+++ Day32.java
@@ -0,0 +1,11 @@
+public class Day32 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = 20;
+
+        System.out.println(a > b || b > c);
+        System.out.println(a < b || c < b);
+        System.out.println(a == 10 || c == 15);
+    }
+}
