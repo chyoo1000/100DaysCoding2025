3a2ddfe6504c8fe2e0e1d2e472450ae2b77f9cbc
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-10-14 22:14:34 +0800

    Day37.java

---
+++ Day37.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Day37{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("input angka: ");
+		int i = in.nextInt();
+		if(i%2 == 0){System.out.println("bilangan genap");
+		}else{System.out.println("bilangan ganjil");}}}
