4253ff4a0594e58910bb0ce4ca3b6b5b3963b70c
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-10-22 22:10:22 +0800

    Day45.java

---
+++ Day45.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Day45 {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.print("masukkan angka (1-7): ");
+        int a = x.nextInt();
+        switch (a) {
+            case 1:
+                System.out.println("Senin");
+                break; 
+            case 2:
+                System.out.println("Selasa");
+                break; 
+            case 3:
+                System.out.println("Rabu");
+                break;
+            case 4:
+                System.out.println("Kamis");
+                break;
+            case 5:
+                System.out.println("Jumat");
+                break;
+            case 6:
+                System.out.println("Sabtu");
+                break;
+            case 7:
+                System.out.println("Minggu");
+                break;
+            default:
+                System.out.println("Pilihan tidak valid");
+        }
+    }
+}
