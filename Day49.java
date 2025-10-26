87605ef7bce5e78b39278a5344ce79a1f6cac8c7
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-10-26 20:15:36 +0800

    Day49.java

---
+++ Day49.java
@@ -0,0 +1,7 @@
+public class Day49 {
+    public static void main(String[] args) {
+        int angka = 5;
+        String hasil = (angka % 2 == 0) ? "Genap" : "Ganjil";
+        System.out.println("Angka ini adalah " + hasil);
+    }
+}
