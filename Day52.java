6cf5ec9849c376e6a28eaeff714700fa6e191cf8
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-10-29 20:03:07 +0800

    Day52.java

---
+++ Day52.java
@@ -0,0 +1,10 @@
+public class Day52 {
+    public static void main(String[] args) {
+        int a = 1;
+
+        while (a <= 5) {
+            System.out.println(a);
+            a++;
+        }
+    }
+}
