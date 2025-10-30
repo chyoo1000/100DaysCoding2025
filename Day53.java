c478d67bb07a02ab1b26c76e7264c5cfbced1bef
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-10-30 22:20:53 +0800

    Day53.java

---
+++ Day53.java
@@ -0,0 +1,17 @@
+public class Day53 {
+	public static void main(String[] args) {
+		int angka = 2;
+
+switch (angka) {
+    case 1:
+        System.out.println("Satu");
+        break;
+    case 2:
+        System.out.println("Dua");
+        break;
+    case 3:
+        System.out.println("Tiga");
+        break;
+}
+	}
+}
