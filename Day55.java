a35c29ab0e07827cc996d40cba96514b3ad51021
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-11-01 23:58:40 +0800

    Create Day55.java

---
+++ Day55.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Day55 {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+
+        System.out.print("Masukkan angka: ");
+        int angka = x.nextInt();
+
+        String hasil = (angka % 2 == 0) ? "Genap" : "Ganjil";
+
+        System.out.println("Angka " + angka + " adalah " + hasil);
+    }
+}
