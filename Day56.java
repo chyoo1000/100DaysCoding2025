fac21b5a91b228dd34156b31e447fe695ee18378
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-11-02 18:25:29 +0800

    Day56.java

---
+++ Day56.java
@@ -0,0 +1,10 @@
+public class Day56.java {
+    public static void main(String[] args) {
+        int i = 1;
+        
+        do {
+            System.out.println("nilai i: " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
