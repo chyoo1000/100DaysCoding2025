08f3b3d32611f0b50f9ac80dd5bcb7ec5d17a0f0
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-11-03 16:57:57 +0800

    Day57.java

---
+++ Day57.java
@@ -0,0 +1,9 @@
+public class Day57 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) { // loop luar
+            for (int j = 1; j <= 2; j++) { // loop dalam
+                System.out.println("i = " + i + ", j = " + j);
+            }
+        }
+    }
+}
