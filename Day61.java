64705e7afd55d4c026c5dbeada72b38d6203917c
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-11-07 22:50:33 +0800

    Day61.java

---
+++ Day61.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Day61 {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.print("Masukkan nilai N: ");
+        int N = x.nextInt();
+        System.out.print("Masukkan nilai M: ");
+        int M = x.nextInt();
+
+        System.out.println("Bilangan kelipatan " + M + " dari 1 sampai " + N + ":");
+        for (int i = 1; i <= N; i++) {
+            if (i % M == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
