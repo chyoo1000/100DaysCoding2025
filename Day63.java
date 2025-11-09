b4923f25b9808a993d002a75dac0479e0fd54808
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-11-09 17:28:54 +0800

    Day63.java

---
+++ Day63.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Day63 {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.print("Masukkan nilai N: ");
+        int N = x.nextInt();
+        int hasil = 1;
+
+        for (int i = 1; i <= N; i++) {
+            hasil *= i;
+        }
+
+        System.out.println("Hasil perkalian dari 1 sampai " + N + " adalah: " + hasil);
+    }
+}
