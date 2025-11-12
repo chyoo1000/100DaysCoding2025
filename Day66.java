4cea54680f84a8a2e260777310aadbe4ec2010f5
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-11-12 20:41:33 +0800

    Day66.java

---
+++ Day66.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Day66 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jumlah baris: ");
+        int n = input.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            System.out.println("*");
+        }
+    }
+}
