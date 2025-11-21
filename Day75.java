3dfe13d0322e5d10db8571bf021c10ad403c1cfd
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-11-21 20:11:37 +0800

    Day75.java

---
+++ Day75.java
@@ -0,0 +1,8 @@
+public class Day75 {
+	public static void main(String[] args) {
+		String a = "hello";
+		System.out.println(a.length());
+		System.out.println(a.toUpperCase());
+		System.out.println(a.toLowerCase());
+	}
+}
