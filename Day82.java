8a62e23766f3b6d997585635d9d5479be64ceae5
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-11-28 21:18:36 +0800

    Day82.java

---
+++ Day82.java
@@ -0,0 +1,8 @@
+public class Day82 {
+	public static void main(String[] args) {
+		String[] warna = {"merah", "kuning", "hijau", "biru"};
+		for(int x = 0; x < warna.length; x++){
+			System.out.println(warna[x]);
+		}
+	}
+}
