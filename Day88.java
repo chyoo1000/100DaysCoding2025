a4f35869a5235fe5be52deecc6af3fbcac8c31cd
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-12-04 21:47:49 +0800

    Day88.java

---
+++ Day88.java
@@ -0,0 +1,8 @@
+public class Day88 {
+    public static void main(String[] args) {
+    String[] angka = {"satu", "dua", "tiga", "empat", "lima"};
+    for(String yoiu : angka){
+        System.out.println(yoiu);
+    }
+}
+}
