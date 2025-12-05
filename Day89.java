58fb65d0b96e9bf9dd7597517ca079c0b0562492
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-12-05 22:28:36 +0800

    Day89.java

---
+++ Day89.java
@@ -0,0 +1,8 @@
+public class Day89 {
+    static void oi() {System.out.println("yoiu");}
+    public static void main(String[] args) {
+        oi();
+        System.out.println("hay");
+        oi();
+}
+}
