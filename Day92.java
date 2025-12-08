b52948c417238a1c825752d8186600e474b45638
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-12-08 19:27:55 +0800

    Day92.java

---
+++ Day92.java
@@ -0,0 +1,9 @@
+public class Day92 {
+    static int kaliDua(int x){
+        return x * 2;  
+    }
+    public static void main(String[] args){
+        int hasil = kaliDua(5);
+        System.out.println(hasil);
+}
+}
