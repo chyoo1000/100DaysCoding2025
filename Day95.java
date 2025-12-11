a0d67e4d306015d22bf10ff3a63a4e8362b71b42
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-12-11 23:11:19 +0800

    Day95.java

---
+++ Day95.java
@@ -0,0 +1,10 @@
+public class Day95 {
+	static void ini(String hewan){
+		System.out.println("ini "+ hewan);
+	}
+	public static void main(String[] args) {
+		ini("anjing");
+		ini("ayam");
+		ini("kucing");
+	}
+}
