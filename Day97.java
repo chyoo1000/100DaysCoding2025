3d460b28348822b21cd33f4f810358898565bda3
Cahyo Susanto Basuki_D0225350 <134618698+chyoo1000@users.noreply.github.com>
2025-12-13 23:42:52 +0800

    Day97.java

---
+++ Day97.java
@@ -0,0 +1,9 @@
+public class Day97 {
+    static int luasPersegi(int sisi) {
+        return sisi * sisi;
+    }
+    public static void main(String[] args) {
+        int hasil = luasPersegi(5);
+        System.out.println("Luas persegi = " + hasil);
+    }
+}
